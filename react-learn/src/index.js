81f68c8218aba7d654296e20a934c756b4001798
fslong <fsl470657570@outlook.com>
2018-08-21 21:21:53 +0800

    心情不怎么开心

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,60 +1,104 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDOM from 'react-dom';
+import  React, { Component } from 'react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+export class Name extends Component {
+	render() {
+		return (
+			<h1>{this.props.name}</h1>
+		)
+	}
+}
 
-function FormattedDate(props){
-    return <h2>现在是{props.date.toLocaleDateString()}{props.date.toLocaleTimeString()}</h2>
-};
-class Clock extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={date:new Date()};
-    };
-    componentDidMount(){
-        this.timerID=setInterval(
-            ()=>this.tick(),
-            1000
-        );
-    };
-    componentWillUnmount(){
-        clearInterval(this.timerID);
-    };
-    tick(){
-        this.setState({
-            date:new Date()
-        });
-    };
-    render(){
-        return(
-            <div>
-                <h1>Hello,world!</h1>
-                <FormattedDate date={this.state.date}/>
-            </div>
-        );
-    };
-};
-ReactDOM.render(
-   <div>
-        <Clock/>
-        <Clock/>
-        <Clock/>
-   </div>,
-    document.getElementById('example')
-);
+export class Link extends Component {
+	render() {
+		return (
+			<a href={this.props.site}>
+				{this.props.site}
+			</a>
+		)
+	}
+}
 
 
-
-function HelloMessage(props){
-    return <h1>Hello {props.name}!</h1>
-};
-const element=<HelloMessage name='fslong'/>;
+export default class Website extends Component {
+	constructor() {
+		super()
+	
+		this.state = {
+			name:"菜鸟教程",
+			site:"https://www.runoob.com"			 
+		}
+	}
+	
+  render() {
+    return (
+      <div>
+				<Name name={this.state.name}/>
+				<Link site={this.state.site}/>        
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-    element,
-    document.getElementById('example')
-);
+	<Website/>,
+	document.getElementById('root')
+)
+
+class Toggle extends Component {
+	constructor(props) {
+		super(props)	
+		this.state = {isToggleOn:true};		
+}
+	handleClick(para){
+		this.setState(prevState =>({
+			isToggleOn:!prevState.isToggleOn
+		}));
+		alert('hello,'+para)
+	}
+	
+	render() {
+		return (
+			<button onClick={this.handleClick.bind(this,'fslong')}>
+				{this.state.isToggleOn ? 'ON':'OFF'}{/*使用三元比较，如果tistoggleon为true那么这里显示on其余显示off*/}
+			</button>
+		)
+	}
+}
+ReactDOM.render(
+	<Toggle />,
+	document.getElementById('example')
+)
+
+
+function UserGreeting(params) {
+	return <h1>欢迎回来!</h1>;
+}
+function GuestGreeting(params) {
+	return <h1>请先注册!</h1>;
+}
+function Greeting(params) {
+	const isLooggedIn=params.isLooggedIn;
+	if (isLooggedIn){
+		return <UserGreeting/>;
+	}
+	else{
+		return <GuestGreeting/>;
+	}
+}
+ReactDOM.render(
+	<Greeting isLooggedIn={true}/>,
+	document.getElementById('example1')
+)
+
+export class LoginControl extends Component {
+	handle
+	render() {
+		return (
+			<div>
+				
+			</div>
+		)
+	}
+}
